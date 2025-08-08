e66ae8e779530665791e678ea555a465bf7256b2
EliasSherem <sheremelias@gmail.com>
2025-08-08 11:44:00 -0600

    correccion de alineamiento de boton en form

--- app/registro/page.tsx
+++ app/registro/page.tsx
@@ -80,7 +80,7 @@ export default function RegistroPage() {
                         />
                       </div>
                       
-                    
+                    </div>
                     <div>
                       <Label htmlFor="email" className="text-gray-300 flex items-center">
                         <Mail className="w-4 h-4 mr-2" />
@@ -126,7 +126,7 @@ export default function RegistroPage() {
                           Procesando...
                         </div>
                       ) : (
-                        <div className="flex items-center">
+                        <div className="flex items-center justify-center">
                           <span>Comenzar Ahora</span>
                           <ArrowRight className="ml-2 w-5 h-5" />
                         </div>
